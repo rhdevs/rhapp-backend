3421d5bfa6a6532428696b3331aa1b1b51db4ed7
simonjulianl <simonjulian21@gmail.com>
2021-03-24 22:19:13 +0800

    feature : push the laundry file server

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+
+
 from flask import Flask
+from flask_cors import CORS, cross_origin
+import os
+import pymongo
 from Laundry.LaundryAPI import laundry_api
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = "Content-Type"
+app.config['SECRET_KEY'] = os.getenv('AUTH_SECRET_KEY')
 
-app.register_blueprint(laundry_api)
+app.register_blueprint(laundry_api, url_prefix="/laundry")
+
 
 @app.route("/")
 def hello():
     return "hello backend"
 
+
 if __name__ == "__main__":
-    app.run('0.0.0.0', 8080)
+    app.run()
