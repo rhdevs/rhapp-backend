4029cf0a7adc8fdae679b987aab4fc63d22e28e1
angnobel <nobelang78@gmail.com>
2021-03-31 21:39:36 +0800

    Add blueprint changes

--- FacilityBooking/FacilitiesAPI.py
+++ FacilityBooking/FacilitiesAPI.py
@@ -1,51 +1,28 @@
-from flask import Flask, request, make_response
-from flask_cors import CORS, cross_origin
-import pymongo
-import json
+from db import *
+from flask import Flask, render_template, flash, redirect, url_for, request, jsonify, make_response
+import os
 from datetime import datetime
-from threading import Thread
+import time
+from flask_cors import cross_origin
+from flask import Blueprint
+import pymongo
+import sys
+sys.path.append("../db")
+
+facilities_api = Blueprint("faciliteis", __name__)
 
 
-def removeObjectID(xs):
-    for i, item in enumerate(xs, start=0):
-        del xs[i]["_id"]
-    return xs
-
-
-def listToIndexedDict(xs):
-    output = {}
-    for i, item in enumerate(xs, start=0):
-        del xs[i]["_id"]
-        output[i] = item
-    return output
-
-
-# MongoDB
-myclient = client = pymongo.MongoClient(
-    "mongodb+srv://rhdevs-db-admin:rhdevs-admin@cluster0.0urzo.mongodb.net/RHApp?retryWrites=true&w=majority")
-db = myclient["RHApp"]
-
-# Flask
-app = Flask("rhapp")
-CORS(app, resources={r"/*": {"origins": "*"}})
-# resources allowed to be accessed explicitly
-# response.headers.add("Access-Control-Allow-Origin", "*"), add this to all responses
-# if the cors still now working
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-
-@app.route('/')
+@facilities_api.route('/')
 @cross_origin()
 def root_route():
     return 'What up losers'
 
 
-@app.route('/facilities', methods=["GET"])
+@facilities_api.route('/facilities', methods=["GET"])
 @cross_origin(supports_credentials=True)
 def all_facilities():
     try:
-        data = removeObjectID(
-            list(db.Facilities.find().sort([("facilityName", 1)])))
+        data = list(db.Facilities.find({"_id": 0}).sort([("facilityName", 1)]))
         response = {"status": "success", "data": {"facilities": data}}
     except Exception as e:
         print(e)
@@ -53,12 +30,12 @@ def all_facilities():
     return make_response(response)
 
 
-@ app.route('/facilities/<facilityID>', methods=["GET"])
+@ facilities_api.route('/facilities/<facilityID>', methods=["GET"])
 @ cross_origin(supports_credentials=True)
 def get_facility_name(facilityID):
     try:
-        data = removeObjectID(
-            list(db.Facilities.find({"facilityID": int(facilityID)})))
+        data = list(db.Facilities.find(
+            {"facilityID": int(facilityID)}, {"_id": 0}))
         if len(data) == 0:
             raise Exception("Facility not found")
         response = {"status": "success", "data": data}
@@ -67,12 +44,12 @@ def get_facility_name(facilityID):
     return make_response(response)
 
 
-@ app.route('/bookings/<bookingID>', methods=["GET"])
+@ facilities_api.route('/bookings/<bookingID>', methods=["GET"])
 @ cross_origin(supports_credentials=True)
 def get_one_booking(bookingID):
     try:
-        data = removeObjectID(
-            list(db.Bookings.find({"bookingID": int(bookingID)})))
+        data = list(db.Bookings.find(
+            {"bookingID": int(bookingID)}, {"_id": 0}))
         response = {"status": "success", "data": data}
         if len(data) == 0:
             raise Exception("Booking not found")
@@ -81,24 +58,24 @@ def get_one_booking(bookingID):
     return make_response(response)
 
 
-@ app.route('/bookings/user/<userID>', methods=["GET"])
+@ facilities_api.route('/bookings/user/<userID>', methods=["GET"])
 @ cross_origin(supports_credentials=True)
 def user_bookings(userID):
     try:
-        data = removeObjectID(list(db.Bookings.find(
-            {"userID": userID}).sort([("startTime", 1)])))
+        data = list(db.Bookings.find({"userID": userID}, {
+                    "_id": 0}).sort([("startTime", 1)]))
         response = {"status": "success", "data": data}
     except Exception as e:
         return {"err": str(e), "status": "failed"}, 400
     return make_response(response)
 
 
-@ app.route('/bookings/facility/<facilityID>/', methods=["GET"])
+@ facilities_api.route('/bookings/facility/<facilityID>/', methods=["GET"])
 @ cross_origin(supports_credentials=True)
 def check_bookings(facilityID):
     try:
-        data = removeObjectID(list(db.Bookings.find({"facilityID": int(facilityID), "startTime": {"$gte": int(
-            request.args.get('startTime'))}, "endTime": {"$lte": int(request.args.get('endTime'))}}).sort([("startTime", 1)])))
+        data = list(db.Bookings.find({"facilityID": int(facilityID), "startTime": {"$gte": int(
+            request.args.get('startTime'))}, "endTime": {"$lte": int(request.args.get('endTime'))}}, {"_id": 0}).sort([("startTime", 1)]))
         response = {"status": "success", "data": data}
     except Exception as e:
         return {"err": str(e), "status": "failed"}, 400
@@ -106,7 +83,7 @@ def check_bookings(facilityID):
     return make_response(response)
 
 
-@ app.route('/bookings', methods=['POST'])
+@ facilities_api.route('/bookings', methods=['POST'])
 @ cross_origin(supports_credentials=True)
 def add_booking():
     try:
@@ -123,12 +100,12 @@ def add_booking():
         if (formData["endTime"] < formData["startTime"]):
             raise Exception("End time eariler than start time")
 
-        conflict = removeObjectID(list(db.Bookings.find({"facilityID": formData.get("facilityID"),
-                                                         "endTime": {
+        conflict = list(db.Bookings.find({"facilityID": formData.get("facilityID"),
+                                          "endTime": {
             "$gte": formData.get('startTime')},
             "startTime": {
             "$lte": formData.get('endTime')}
-        })))
+        }, {"_id": 0}))
 
         if (len(conflict) != 0):
             raise Exception("Conflict Booking")
@@ -149,7 +126,7 @@ def add_booking():
     return make_response(response)
 
 
-@ app.route('/bookings/<bookingID>', methods=['GET'])
+@ facilities_api.route('/bookings/<bookingID>', methods=['GET'])
 @ cross_origin(supports_credentials=True)
 def get_booking(bookingID):
     try:
@@ -162,7 +139,7 @@ def get_booking(bookingID):
     return make_response(response)
 
 
-@ app.route('/bookings/<bookingID>', methods=['PUT'])
+@ facilities_api.route('/bookings/<bookingID>', methods=['PUT'])
 @ cross_origin(supports_credentials=True)
 def edit_booking(bookingID):
     try:
@@ -188,7 +165,7 @@ def edit_booking(bookingID):
     return make_response(response)
 
 
-@ app.route('/bookings/<bookingID>', methods=['DELETE'])
+@ facilities_api.route('/bookings/<bookingID>', methods=['DELETE'])
 @ cross_origin(supports_credentials=True)
 def delete_booking(bookingID):
     try:
@@ -208,7 +185,7 @@ def delete_booking(bookingID):
     return make_response(response)
 
 
-@ app.route('/users/telegramID/<userID>', methods=["GET"])
+@ facilities_api.route('/users/telegramID/<userID>', methods=["GET"])
 @ cross_origin(supports_credentials=True)
 def user_telegram(userID):
     try:
@@ -223,19 +200,3 @@ def user_telegram(userID):
         print(e)
         return {"err": str(e), "status": "failed"}, 400
     return make_response(response)
-
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-
-keep_alive()
-
-# if __name__ == '__main__':
-#   keep_alive();
-#   app.run('0.0.0.0', port=8080)
